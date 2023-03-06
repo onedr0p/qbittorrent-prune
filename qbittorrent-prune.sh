71c4857addf05b4f158b8c45a0414b5319138a7f
heathlarsen <68149150+heathlarsen@users.noreply.github.com>
2023-03-07 02:46:56 +1300

    Updated regex to support POSIX regex
    Bash does not support using \d. Instead [0-9] must be used.

--- qbittorrent-prune.sh
+++ qbittorrent-prune.sh
@@ -111,7 +111,7 @@ do
     # Delete the torrent
     if [ "${DRY_RUN}" == "false" ]; then
         http_response_code=$(${CURL_CMD} -X POST -o /dev/null -w "%{http_code}" --cookie "SID=${cookie}" -d "hashes=${failed_torrent_hash}&deleteFiles=${QB_DELETE_FILES}" "${api_url}/torrents/delete")
-        valid_response_regex="(2|3)[\d]{2}"
+        valid_response_regex="(2|3)[0-9]{2}"
         if [[ ! "${http_response_code}" =~ ${valid_response_regex} ]]; then
             [[ ${LOG_LEVEL} -ge 1 ]] && echo "$(date -u) - ERROR: Unable to delete torrent ${torrent_name}"
             [[ ${LOG_LEVEL} -ge 1 ]] && echo "$(date -u) - ERROR: Invalid HTTP Status Code: ${http_response_code}"
